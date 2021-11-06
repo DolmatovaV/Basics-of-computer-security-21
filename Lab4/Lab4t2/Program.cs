0c3b308a0c96a4dc577ec4799528733dbffa2b6c
Cassiopeia <dolmatowit@gmail.com>
2021-11-06 16:31:30 +0200

    Lab 4

---
+++ Lab4/Lab4t2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lab4t2
+{
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var randomNumberGenerator =
+            new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+
+        public static byte[] HashPasswordSHA512(byte[] toBeHashed, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAlgorithmName.SHA512))
+            {
+                return rfc2898.GetBytes(64);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string passwordToHash = "Password";
+            HashPassword(passwordToHash, 100000, 1); //10 variant = 100 000
+            HashPassword(passwordToHash, 150000, 2);   //100 000 + 50 000
+            HashPassword(passwordToHash, 200000, 3);
+            HashPassword(passwordToHash, 250000, 4);
+            HashPassword(passwordToHash, 300000, 5);
+            HashPassword(passwordToHash, 350000, 6);
+            HashPassword(passwordToHash, 400000, 7);
+            HashPassword(passwordToHash, 450000, 8);
+            HashPassword(passwordToHash, 500000, 9);
+            HashPassword(passwordToHash, 550000, 10);
+            Console.ReadLine();
+        }
+        private static void HashPassword(string passwordToHash,
+        int numberOfRounds, int counter)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var hashedPassword = PBKDF2.HashPasswordSHA512(
+            Encoding.UTF8.GetBytes(passwordToHash),
+            PBKDF2.GenerateSalt(),
+            numberOfRounds);
+            sw.Stop();
+            
+            Console.WriteLine("Password to hash : " + passwordToHash);
+            Console.WriteLine("Hashed Password : " +
+            Convert.ToBase64String(hashedPassword));
+            Console.WriteLine(counter + ")" + " Iterations <" + numberOfRounds + ">Elapsed Time: " + sw.ElapsedMilliseconds + "ms");
+            Console.WriteLine();
+
+        }
+
+    }
+    }
+
---
+++ Lab4/Lab4t3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lab4t3
+{
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var randomNumberGenerator =
+            new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public static byte[] HashPassword(byte[] toBeHashed,
+        byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(
+            toBeHashed, salt, numberOfRounds))
+            {
+                return rfc2898.GetBytes(20);
+            }
+        }
+        public static byte[] HashPasswordSHA512(byte[] toBeHashed, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAlgorithmName.SHA512))
+            {
+                return rfc2898.GetBytes(64);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] salt = PBKDF2.GenerateSalt();
+            Console.Write("Enter your name:");
+            string registerName = Console.ReadLine();
+            Console.Write("Enter your password:");
+            var scriptedPW = PBKDF2.HashPasswordSHA512(Encoding.Unicode.GetBytes(Console.ReadLine()), salt, 100000);
+            Console.WriteLine($"(In order to see the hash {Convert.ToBase64String(scriptedPW)})");
+            Console.WriteLine("Register completed");
+
+
+            Console.WriteLine("Enter your name for autorization:");
+            string authName = Console.ReadLine();
+            Console.WriteLine("Enter your password for autorization:");
+            var authScriptedPW = PBKDF2.HashPasswordSHA512(Encoding.Unicode.GetBytes(Console.ReadLine()), salt, 100000);
+            Console.WriteLine($"(In order to see the hash {Convert.ToBase64String(authScriptedPW)})");
+            if (registerName == authName && scriptedPW.Length == authScriptedPW.Length)
+            {
+                int i = 0;
+                while ((i < scriptedPW.Length) && (scriptedPW[i] == authScriptedPW[i]))
+                {
+                    i++;
+                }
+                if (i == scriptedPW.Length)
+                {
+                    Console.WriteLine("Authorization successful");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong login or password");
+                }
+            }
+            else
+            {
+                 Console.WriteLine("Wrong login or password");
+            }
+        }
+    }
+    }
+
